0ef9012b578bcecdab0eb490b48d1d3370e2c97e
NahomMA <72306632+NahomMA@users.noreply.github.com>
2023-01-30 22:23:08 -0700

    Update sub.js

--- sub.js
+++ sub.js
@@ -1,7 +1,7 @@
 // MQTT subscriber
 
 const mqtt=require("mqtt")
-const client=mqtt.connect('mqtt://192.168.1.65:1883')
+const client=mqtt.connect('mqtt://hostIP:1883')
 const topic="iotdevice-1"
 client.on("message",(topic, message)=>{
     message=message.toString()
